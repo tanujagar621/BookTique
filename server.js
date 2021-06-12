87760d29bc491f840f8504bce0445f3a335fa6c0
tanujagar621 <tanujagarwal621@gmail.com>
2021-06-12 22:38:54 +0530

    adding project to git

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-// if(process.env.NODE_ENV != 'production')
-// {
-//     require('dotenv').parse();
-// }
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const port = process.env.PORT || 3000;
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+// const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 
 app.use(expressLayouts);
+// app.use(express.static(__dirname + "/public"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/library", {
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on('error', error => console.error(error))
-db.once('open', () => console.log("connected to mongoose"))
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to mongoose"));
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(port);
